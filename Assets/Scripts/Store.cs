f2724dc2a201a9c1264a9c765a2c19675c3ba2a6
Danny Tizon <dtizon@gmail.com>
2016-04-10 15:59:02 -0700

    basic timer added

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -10,18 +10,33 @@ public class Store : MonoBehaviour {
 	int storeCount;
 	float currentBalance;
 	float baseStoreCost;
+	float baseStoreProfit;
+	float storeTimer = 4f;
+	float currentTimer = 0f;
+	bool startTimer;
 
 	// Use this for initialization
 	void Start () {
 		storeCount = 1;
 		currentBalance = 2.0f;
 		baseStoreCost = 1.50f;
+		baseStoreProfit = .50f;
+		startTimer = false;
 		CurrentBalanceText.text = currentBalance.ToString("C2");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (startTimer){
+			currentTimer += Time.deltaTime;
+			if (currentTimer > storeTimer) {
+				Debug.Log ("timer has ended - resetting!");
+				startTimer = false;
+				currentTimer = 0f;
+				currentBalance += baseStoreProfit * storeCount;
+				CurrentBalanceText.text = currentBalance.ToString("C2");
+			}
+		}		
 	}
 
 	public void BuyStore() {
@@ -34,6 +49,12 @@ public class Store : MonoBehaviour {
 		StoreCountText.text = storeCount.ToString();
 		currentBalance = currentBalance - baseStoreCost;
 		CurrentBalanceText.text = currentBalance.ToString("C2");
-		Debug.Log ("currentBalance=" + currentBalance);
+	}
+
+	public void StoreOnClick(){
+		Debug.Log ("storeOnClick!");
+		if (!startTimer) {
+			startTimer = true;	
+		}
 	}
 }
