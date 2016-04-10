29672546b06642ca2214de1d6e50d8ec8144e8ae
Danny Tizon <dtizon@gmail.com>
2016-04-10 15:08:05 -0700

    store counter for buy button

---
+++ Assets/Scripts/Store.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Store : MonoBehaviour {
+
+	public Text StoreCountText;
+
+	int storeCount;
+
+	// Use this for initialization
+	void Start () {
+		storeCount = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void BuyStore() {
+		storeCount++;
+		StoreCountText.text = storeCount.ToString();
+	}
+}
