ec7fa8c8479d5ae7a6e89266e8d6554d1868c8d2
Danny Tizon <dtizon@gmail.com>
2016-04-10 15:29:26 -0700

    display current balance

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 public class Store : MonoBehaviour {
 
 	public Text StoreCountText;
+	public Text CurrentBalanceText;
 
 	int storeCount;
+	float currentBalance;
+	float baseStoreCost;
 
 	// Use this for initialization
 	void Start () {
 		storeCount = 1;
+		currentBalance = 2.0f;
+		baseStoreCost = 1.50f;
+		CurrentBalanceText.text = currentBalance.ToString("C2");
 	}
 	
 	// Update is called once per frame
@@ -19,7 +25,15 @@ public class Store : MonoBehaviour {
 	}
 
 	public void BuyStore() {
+		if (baseStoreCost > currentBalance) {
+			Debug.Log ("Not enough money!");
+			return;
+		}
+
 		storeCount++;
 		StoreCountText.text = storeCount.ToString();
+		currentBalance = currentBalance - baseStoreCost;
+		CurrentBalanceText.text = currentBalance.ToString("C2");
+		Debug.Log ("currentBalance=" + currentBalance);
 	}
 }
