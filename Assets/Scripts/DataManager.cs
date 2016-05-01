3bd7ad9c157c54f08b29f43e4a65d1ff1d1a9999
Danny Tizon <dtizon@gmail.com>
2016-05-01 10:58:21 -0700

    initial setup of reading data from xml file

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+
+public class DataManager : MonoBehaviour {
+
+	public TextAsset GameData;
+
+	public void Start() {
+		Invoke ("LoadData", .1f);
+	}
+
+	public void LoadData() {
+		Debug.Log ("here!");
+
+		//for json but will probably need some DTO instead of mapping directly to Stores class
+		//YouObject[] objects = JsonHelper.getJsonArray<YouObject> (jsonString);
+
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml (GameData.text);
+
+		XmlNodeList storeList = xmlDoc.GetElementsByTagName ("store");
+		foreach (XmlNode storeNode in storeList) {
+
+			XmlNodeList storeNodes = storeNode.ChildNodes;
+
+			foreach (XmlNode store in storeNodes) {
+				Debug.Log (store.Name);
+				Debug.Log (store.InnerText);	
+			}
+
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/JsonHelper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JsonHelper : MonoBehaviour {
+
+	public static T[] getJsonArray<T>(string json)
+	{
+		string newJson = "{ \"array\": " + json + "}";
+		Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>> (newJson);
+		return wrapper.array;
+	}
+
+	[System.Serializable]
+	private class Wrapper<T>
+	{
+		public T[] array;
+	}
+}
